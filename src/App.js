5b4549ae185d712227a9ab322d0adfc074056fef
Ranjith Samala <samalaranjith1@gmail.com>
2023-11-22 17:33:48 +0530

    latest commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Git New branch</p>
+      <p>Git New branch</p>
+      <p>LatestBranch</p>
+    </div>
+  );
+}
+
+export default App;
