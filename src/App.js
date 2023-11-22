8529ee295844d984882af3e43b3bc6fd0a2335aa
Ranjith Samala <samalaranjith1@gmail.com>
2023-11-22 17:33:48 +0530

    an updated commit message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Git master branch</p>
+      <p>This is a master branch after stash 1 Hi ammending </p>
+    </div>
+  );
+}
+
+export default App;
