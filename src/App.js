fb8ac0f9595d9cfe96b55213fe666e02e6282736
Ranjith Samala <samalaranjith1@gmail.com>
2023-12-21 19:06:27 +0530

    newBranch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Git master branch</p>
+      <p>Git master branch this new branch and master combo</p>
       <p>This is a master branch after stash 1</p>
     </div>
   );
