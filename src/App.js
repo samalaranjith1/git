0b6e2ac49f29024a8321a359be4e068c603c3609
Ranjith Samala <samalaranjith1@gmail.com>
2024-05-27 08:11:18 +0530

    Revert "Revert "i will come to here""
    This reverts commit 081193a9538b8bb45548155cc91bea9973ed5127.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>give going to revert to this command</p>
-      <p>after one comit</p>
-      <p>after two comit</p>
+      <p>I will be revert to this commit </p>
+      <p>I am going to revert to this commit </p>
+      <p>I will be revert to this commit </p>
+      <p>I am going to revert to this commit </p>
+      <p>I will be revert to this commit </p>
+      <p>I am going to revert to this commit </p>
+      <p>I will be revert to this commit </p>
+      <p>I am going to revert to this commit </p>
     </div>
   );
 }
