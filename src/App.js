39942d2fed9d246bd3e5e1a7b83303c28bbfd36c
Ranjith Samala <samalaranjith1@gmail.com>
2024-05-27 07:47:09 +0530

    Revert "i want to roll back to last commit"
    This reverts commit 82293cf23411cc4bc879828dd066ee9e91f7a7bd.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>give going to revert to this command</p>
+      <p>give trying revert command</p>
     </div>
   );
 }
