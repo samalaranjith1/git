446a2c94ce62a50f8c510c435f495fe2006110b0
Ranjith Samala <samalaranjith1@gmail.com>
2024-06-06 20:37:26 +0530

    moving to diferent branch

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <div className="App">
       <p>I will be revert to this commit </p>
-      <p>I am going to revert to this commit </p>
-      <p>I will be revert to this commit </p>
-      <p>I am going to revert to this commit </p>
-      <p>I will be revert to this commit </p>
-      <p>I am going to revert to this commit </p>
-      <p>I will be revert to this commit </p>
-      <p>I am going to revert to this commit </p>
     </div>
   );
 }
